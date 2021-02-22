fabc8fd69f9dbfe66a3da19a6dce626b6a95d638
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-02-22 10:18:10 -0600

    Create quiz.html and objects for quiz.

---
+++ assets/scripts/script.js
@@ -0,0 +1,26 @@
+const question1 = {
+    question: 'How do you construct an array in Javascript?',
+    choice1: {answer1: 'const array = {1,2,3}', value: false,},
+    choice2: {answer2: 'const array = [1,2,3]', value: true,},
+    choice3: {answer3: 'const array = (1,2,3)', value: false,},
+    choice4: {answer4: 'const array = <1,2,3>', value: false,},
+    
+};
+
+const question2 = {
+    question: 'What is the value passed when clicking "cancel" in a Confirm Box?',
+    choice1: {answer1: 'true', value: false,},
+    choice2: {answer2: 'null', value: false,},
+    choice3: {answer3: 'false', value: true,},
+    choice4: {answer4: 'NaN', value: false,},
+    
+};
+
+const question3 = {
+    question: 'Which array method adds an element to the end of an array?',
+    choice1: {answer1: '.push', value: true,},
+    choice2: {answer2: '.pull', value: false,},
+    choice3: {answer3: '.pop', value: false,},
+    choice4: {answer4: '.shift', value: false,},
+    
+};
