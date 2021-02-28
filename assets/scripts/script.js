04bdc84e2fa24e7817890ebe2e5f31315b68c102
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-02-28 12:32:43 -0600

    Wrap quiz questions into an object.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -21,64 +21,54 @@ const variables = {
     
 };
 
-//The set of questions
-const question1 = {
-    question: 'How do you construct an array in Javascript?',
-    choice1: {answer1: '{1,2,3}', response: 'false',},
-    choice2: {answer2: '[1,2,3]', response: 'true',},
-    choice3: {answer3: '(1,2,3)', response: 'false',},
-    choice4: {answer4: '<1,2,3>', response: 'false',},
+const quizQuestions = {
+    question1: {
+        question: 'How do you construct an array in Javascript?',
+        choice1: {answer1: '{1,2,3}', response: 'false',},
+        choice2: {answer2: '[1,2,3]', response: 'true',},
+        choice3: {answer3: '(1,2,3)', response: 'false',},
+        choice4: {answer4: '<1,2,3>', response: 'false',},},
+    
+    question2: {
+        question: 'What is the value passed when clicking "cancel" in a Confirm Box?',
+        choice1: {answer1: 'true', response: 'false',},
+        choice2: {answer2: 'null', response: 'false',},
+        choice3: {answer3: 'false', response: 'true',},
+        choice4: {answer4: 'NaN', response: 'false',},},
+    
+    question3: {
+        question: 'Which array method adds an element to the end of an array?',
+        choice1: {answer1: 'join', response: 'false',},
+        choice2: {answer2: 'push', response: 'true',},
+        choice3: {answer3: 'pop', response: 'false',},
+        choice4: {answer4: 'shift', response: 'false',},},
+    
+    question4: {
+        question: 'What unit measures font size relative to the root element?',
+        choice1: {answer1: 'px', response: 'false',},
+        choice2: {answer2: 'vh', response: 'false',},
+        choice3: {answer3: 'em', response: 'false',},
+        choice4: {answer4: 'rem', response: 'true',},},
+    
+    question5: {
+        question: 'Which position property removes an element out of the normal flow of the document?',
+        choice1: {answer1: 'relative', response: 'false',},
+        choice2: {answer2: 'sticky', response: 'false',},
+        choice3: {answer3: 'absolute', response: 'true',},
+        choice4: {answer4: 'static', response: 'false',},},
 
+    question6: {
+        question: 'Which value is falsy in Javascript?',
+        choice1: {answer1: "'true'", response: 'false',},
+        choice2: {answer2: "'false'", response: 'false',},
+        choice3: {answer3: '42', response: 'false',},
+        choice4: {answer4: 'null', response: 'true',},},
     
 };
 
-const question2 = {
-    question: 'What is the value passed when clicking "cancel" in a Confirm Box?',
-    choice1: {answer1: 'true', response: 'false',},
-    choice2: {answer2: 'null', response: 'false',},
-    choice3: {answer3: 'false', response: 'true',},
-    choice4: {answer4: 'NaN', response: 'false',},
-    
-};
-
-const question3 = {
-    question: 'Which array method adds an element to the end of an array?',
-    choice1: {answer1: 'join', response: 'false',},
-    choice2: {answer2: 'push', response: 'true',},
-    choice3: {answer3: 'pop', response: 'false',},
-    choice4: {answer4: 'shift', response: 'false',},
-    
-};
-
-const question4 = {
-    question: 'What unit measures font size relative to the root element?',
-    choice1: {answer1: 'px', response: 'false',},
-    choice2: {answer2: 'vh', response: 'false',},
-    choice3: {answer3: 'em', response: 'false',},
-    choice4: {answer4: 'rem', response: 'true',},
-    
-};
-
-const question5 = {
-    question: 'Which position property removes an element out of the normal flow of the document?',
-    choice1: {answer1: 'relative', response: 'false',},
-    choice2: {answer2: 'sticky', response: 'false',},
-    choice3: {answer3: 'absolute', response: 'true',},
-    choice4: {answer4: 'static', response: 'false',},
-    
-};
-
-const question6 = {
-    question: 'Which value is falsy in Javascript?',
-    choice1: {answer1: "'true'", response: 'false',},
-    choice2: {answer2: "'false'", response: 'false',},
-    choice3: {answer3: '42', response: 'false',},
-    choice4: {answer4: 'null', response: 'true',},
-    
-};
 
 //An array of all the questions
-const questionsArray = [question1, question2, question3, question4, question5, question6];
+const questionsArray = [quizQuestions.question1, quizQuestions.question2, quizQuestions.question3, quizQuestions.question4, quizQuestions.question5, quizQuestions.question6];
 
 //Introduction to the game with Start Game button
 variables.mainHead.innerHTML = 'Coding Quiz Challenge';
